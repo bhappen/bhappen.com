cb0cdbf03d65c09b2ad5d3d9536ded88944a7b0f
BHappen <alexgmyth@outlook.com>
2023-09-07 21:05:23 -0600

    added header

--- bhappen.com/app/layout.tsx
+++ bhappen.com/app/layout.tsx
@@ -1,6 +1,8 @@
+'use client'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from './components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-gray-50 text-gray-950 ${inter.className}`}>{children}</body>
+      <Header />
+      <body className={`bg-gray-50 text-gray-950 ${inter.className}`}>
+        {children}
+      </body>
       <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
       <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
     </html>
--- bhappen.com/app/page.tsx
+++ bhappen.com/app/page.tsx
@@ -1,7 +1,6 @@
+'use client'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main></main>
-  )
+  return <main></main>
 }
